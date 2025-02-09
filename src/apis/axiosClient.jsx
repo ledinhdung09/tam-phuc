3c88317f70e70380b8cea09859635310b9d39023
dung001 <ledinhdung.net@gmail.com>
2025-02-09 19:39:16 +0700

    1

--- src/apis/axiosClient.jsx
+++ src/apis/axiosClient.jsx
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  //baseURL: "http://localhost/Backend-API-Print-Shop/api/",
-  //baseURL: "https://hcrm.online/public/tamphuc/api",
- 
+  baseURL: "https://lumiaicreations.com/tam-phuc/Backend-API-Print-Shop/api/",
   headers: {
     "Content-Type": "application/json",
   },
